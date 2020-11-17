2d5c01fdff9626da5e1e042a73d1bcbec0dafece
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-17 12:14:20 -0300

    actualización jugador + patrón singleton

--- src/logicaEntidades/Jugador.java
+++ src/logicaEntidades/Jugador.java
@@ -1,9 +1,47 @@
 package logicaEntidades;
 
+import visitor.VisitorEntidad;
+
+/**
+ * Clase que modela al personaje principal.
+ *
+ */
 public class Jugador {
+	private static Jugador jugador;
 	
-	public Jugador() {
-		Proyectil p = new Proyectil_Sanitario();
-		super(0,p);
+	/**
+	 * Constructor del jugador.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 * @param proyectil
+	 */
+	private Jugador(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad,Proyectil proyectil) {
+		super(movimiento,grafica,visitor,velocidad,proyectil);
+	}
+	
+	/**
+	 * Método que devuelve al jugador.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 * @param proyectil
+	 * @return
+	 */
+	public static Jugador getJugador(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad,Proyectil proyectil) {
+		if(jugador == null) {
+			jugador = new Jugador(movimiento,grafica,visitor,velocidad,proyectil);
+		}
+		return jugador;
+	}
+	
+	/**
+	 * Visita al jugador.
+	 * @param v Visitor.
+	 */
+	public void visitar(VisitorEntidad v) {
+		v.visitJugador(this);
 	}
 }
