7ded9eeb3e8a83f7b2e402f0539253a33e7e8dc3
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-17 12:59:06 -0300

    actualización premio
    de nuevo no hay métodos abstractos?

--- src/logicaEntidades/Premio.java
+++ src/logicaEntidades/Premio.java
@@ -1,5 +1,20 @@
 package logicaEntidades;
 
-public abstract class Premio {
+import visitor.VisitorEntidad;
 
+/**
+ * Clase que modela un premio.
+ */
+public abstract class Premio extends Entidad {
+	
+	/**
+	 * Constructor del premio.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 */
+	public Premio(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad) {
+		super(movimiento,grafica,visitor,velocidad);
+	}
 }
