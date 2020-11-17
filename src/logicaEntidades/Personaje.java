ff6ba03c173bb407359611daa6b954f4a956a56d
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-17 12:01:42 -0300

    actualización personaje

--- src/logicaEntidades/Personaje.java
+++ src/logicaEntidades/Personaje.java
@@ -1,21 +1,41 @@
 package logicaEntidades;
 
+import visitor.*;
+
+/**
+ * Clase que modela un personaje.
+ */
 public abstract class Personaje extends Entidad {
-	protected int cargaViral;
 	protected Proyectil proyectil;
-	
-	public Personaje(int cargaViral,Proyectil proyectil) {
-		super();
-		this.cargaViral = cargaViral;
+	protected int vida;
+	protected final int vidaMax = 100;
+
+	/**
+	 * Constructor del personaje.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 * @param proyectil
+	 */
+	public Personaje(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad,Proyectil proyectil) {
+		super(movimiento,grafica,visitor);
 		this.proyectil = proyectil;
 	}
 
-	public int getCargaViral() {
-		return cargaViral;
+	public Proyectil getProyectil() {
+		return proyectil;
 	}
 
-	public void setCargaViral(int cargaViral) {
-		this.cargaViral = cargaViral;
+	public void setProyectil(Proyectil proyectil) {
+		this.proyectil = proyectil;
 	}
-	
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
 }
