14e58c40e207a78489bff4b99c33ae0c3d2ecc6a
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-17 12:27:49 -0300

    actualización proyectil
    no deberían haber métodos abstractos?

--- src/logicaEntidades/Proyectil.java
+++ src/logicaEntidades/Proyectil.java
@@ -1,5 +1,32 @@
 package logicaEntidades;
 
+import visitor.VisitorEntidad;
+
+/**
+ * Clase que modela un proyectil.
+ */
 public abstract class Proyectil extends Entidad {
+	protected int poder;
+	
+	/**
+	 * Constructor del proyectil.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 * @param poder
+	 */
+	public Proyectil(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad,int poder) {
+		super(movimiento,grafica,visitor,velocidad);
+		this.poder = poder;
+	}
+
+	public int getPoder() {
+		return poder;
+	}
+
+	public void setPoder(int poder) {
+		this.poder = poder;
+	}
 	
 }
