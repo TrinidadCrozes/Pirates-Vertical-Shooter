0cf0f3c929bf8468c1d496adca7c4e6bfab94fc8
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-17 12:38:47 -0300

    actualización proyectil sanitario súper

--- src/logicaEntidades/Proyectil_Sanitario_Super.java
+++ src/logicaEntidades/Proyectil_Sanitario_Super.java
@@ -1,5 +1,29 @@
 package logicaEntidades;
 
-public class Proyectil_Sanitario_Super {
+import visitor.VisitorEntidad;
 
+/**
+ * Clase que modela un proyectil del personaje principal mejorado.
+ */
+public class Proyectil_Sanitario_Super extends Proyectil{
+	
+	/**
+	 * Constructor del proyectil mejorado del jugador principal.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 * @param poder
+	 */
+	public Proyectil_Sanitario_Super(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad,int poder) {
+		super(movimiento,grafica,visitor,velocidad,poder);
+	}
+	
+	/**
+	 * Visita al proyectil del jugador mejorado.
+	 * @param v Visitor.
+	 */
+	public void visitar(VisitorEntidad v) {
+		v.visitProyectil_Sanitario_Super(this);
+	}
 }
