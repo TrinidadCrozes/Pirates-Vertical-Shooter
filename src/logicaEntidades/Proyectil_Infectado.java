4da6f5d4592c06ac4379faa4dd132db2c57c7ca7
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-17 12:39:07 -0300

    Update Proyectil_Infectado.java

--- src/logicaEntidades/Proyectil_Infectado.java
+++ src/logicaEntidades/Proyectil_Infectado.java
@@ -1,5 +1,29 @@
 package logicaEntidades;
 
-public class Proyectil_Infectado {
+import visitor.VisitorEntidad;
 
+/**
+ * Clase que modela un proyectil del enemigo.
+ */
+public class Proyectil_Infectado extends Proyectil{
+	
+	/**
+	 * Constructor del proyectil del enemigo.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 * @param poder
+	 */
+	public Proyectil_Infectado(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad,int poder) {
+		super(movimiento,grafica,visitor,velocidad,poder);
+	}
+	
+	/**
+	 * Visita al proyectil del enemigo.
+	 * @param v Visitor.
+	 */
+	public void visitar(VisitorEntidad v) {
+		v.visitProyectil_Infectado(this);
+	}
 }
