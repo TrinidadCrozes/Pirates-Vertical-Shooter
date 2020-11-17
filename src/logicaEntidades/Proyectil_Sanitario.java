b6332020872015f76a797f2ad255be6d18a8e6f6
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-17 12:38:15 -0300

    actualización proyectil sanitario

--- src/logicaEntidades/Proyectil_Sanitario.java
+++ src/logicaEntidades/Proyectil_Sanitario.java
@@ -1,5 +1,29 @@
 package logicaEntidades;
 
-public class Proyectil_Sanitario extends Proyectil{
+import visitor.VisitorEntidad;
 
+/**
+ * Clase que modela un proyectil del personaje principal.
+ */
+public class Proyectil_Sanitario extends Proyectil{
+	
+	/**
+	 * Constructor del proyectil del jugador principal.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 * @param poder
+	 */
+	public Proyectil_Sanitario(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad,int poder) {
+		super(movimiento,grafica,visitor,velocidad,poder);
+	}
+	
+	/**
+	 * Visita al proyectil del jugador.
+	 * @param v Visitor.
+	 */
+	public void visitar(VisitorEntidad v) {
+		v.visitProyectil_Sanitario(this);
+	}
 }
