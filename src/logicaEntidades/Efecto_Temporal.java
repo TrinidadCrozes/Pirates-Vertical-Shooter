459e2d689a9e2a52114c56d2852b5f89a9921617
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-18 12:42:02 -0300

    actualización premios

--- src/logicaEntidades/Efecto_Temporal.java
+++ src/logicaEntidades/Efecto_Temporal.java
@@ -1,5 +1,29 @@
 package logicaEntidades;
 
-public abstract class Efecto_Temporal extends Premio{
+import visitor.VisitorEntidad;
 
+/**
+ * Clase que modela al tipo de premio efecto temporal.
+ */
+public abstract class Efecto_Temporal extends Premio{
+	protected final int duracion = 10;
+	
+	/**
+	 * Constructor del premio.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 */
+	public Efecto_Temporal(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad) {
+		super(movimiento,grafica,visitor,velocidad);
+	}
+	
+	/**
+	 * Retorna la duracion del efecto temporal.
+	 * @return duracion del efecto.
+	 */
+	public int getDuracion() {
+		return duracion;
+	}
 }
