39807e01bba6b242207b04474fd8b04628fe8d66
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-18 12:41:44 -0300

    actualización premios

--- src/logicaEntidades/Cuarentena_Obligatoria.java
+++ src/logicaEntidades/Cuarentena_Obligatoria.java
@@ -1,5 +1,28 @@
 package logicaEntidades;
 
+import visitor.VisitorEntidad;
+
+/**
+ * Clase que modela al premio cuarentena obligatoria.
+ */
 public class Cuarentena_Obligatoria extends Efecto_Temporal {
 
+	/**
+	 * Constructor del premio.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 */
+	public Cuarentena_Obligatoria(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad) {
+		super(movimiento,grafica,visitor,velocidad);
+	}
+	
+	/**
+	 * Visita al premio.
+	 * @param v Visitor.
+	 */
+	public void visitar(VisitorEntidad v) {
+		v.visitCuarentena_Obligatoria(this);
+	}
 }
