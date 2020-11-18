866b29dddc89092ef88c10559620a03fb07853ef
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-18 12:39:57 -0300

    actualización premios

--- src/logicaEntidades/Super_Arma_Sanitaria.java
+++ src/logicaEntidades/Super_Arma_Sanitaria.java
@@ -1,5 +1,28 @@
 package logicaEntidades;
 
+import visitor.VisitorEntidad;
+
+/**
+ * Clase que modela al premio súper arma sanitaria.
+ */
 public class Super_Arma_Sanitaria extends Efecto_Temporal {
 
+	/**
+	 * Constructor del premio.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 */
+	public Super_Arma_Sanitaria(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad) {
+		super(movimiento,grafica,visitor,velocidad);
+	}
+	
+	/**
+	 * Visita al premio.
+	 * @param v Visitor.
+	 */
+	public void visitar(VisitorEntidad v) {
+		v.visitSuper_Arma_Sanitaria(this);
+	}
 }
