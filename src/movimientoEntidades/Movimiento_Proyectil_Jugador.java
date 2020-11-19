9388c60d54287c384c509f9d4ff082f0841a3d3e
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-19 10:32:59 -0300

    actualización movimiento

--- src/movimientoEntidades/Movimiento_Proyectil_Jugador.java
+++ src/movimientoEntidades/Movimiento_Proyectil_Jugador.java
@@ -1,5 +1,34 @@
 package movimientoEntidades;
 
-public class Movimiento_Proyectil_Jugador extends Movimiento_Proyectil{
+/**
+ * Clase que modela el movimiento de los proyectiles del jugador.
+ */
+public class Movimiento_Proyectil_Jugador extends Movimiento{
+
+	/**
+	 * Constructor del movimiento de un proyectil, se inicializa con la posición inicial del mismo.
+	 * @param x Coordenada x inicial de la entidad.
+	 * @param y Coordenada y inicial de la entidad.
+	 * @param velocidad Velocidad de movimiento de la entidad.
+	 * @param alturaFrame La altura del frame del juego.
+	 */
+	public Movimiento_Proyectil_Jugador(int x, int y, int velocidad, int alturaFrame) {
+		super(x, y, velocidad, alturaFrame);
+	}
+
+	@Override
+	public void desplazar() {
+		this.posicion.setLocation(this.posicion.x,this.posicion.y - velocidad);
+	}
+
+	@Override
+	public void moverDerecha() {
+		//No se mueve hacia la derecha.
+	}
+
+	@Override
+	public void moverIzquierda() {
+		//No se mueve hacia la izquierda.
+	}
 
 }
