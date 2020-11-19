f7aea4da2db0bc193cdb4383853c46ee0ea4b4bc
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-19 10:31:27 -0300

    actualización movimiento

--- src/movimientoEntidades/Movimiento_Jugador.java
+++ src/movimientoEntidades/Movimiento_Jugador.java
@@ -10,9 +10,10 @@ public class Movimiento_Jugador extends Movimiento{
 	 * @param x Coordenada x inicial de la entidad.
 	 * @param y Coordenada y inicial de la entidad.
 	 * @param velocidad Velocidad de movimiento de la entidad.
+	 * @param alturaFrame La altura del frame del juego.
 	 */
-	public Movimiento_Jugador(int x, int y, int velocidad) {
-		super(x, y, velocidad);
+	public Movimiento_Jugador(int x, int y, int velocidad, int alturaFrame) {
+		super(x, y, velocidad, alturaFrame);
 	}
 
 	@Override
@@ -31,3 +32,4 @@ public class Movimiento_Jugador extends Movimiento{
 	}
 
 }
+
