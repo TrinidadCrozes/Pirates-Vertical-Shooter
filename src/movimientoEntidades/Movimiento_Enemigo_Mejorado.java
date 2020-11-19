ce8d03431aa9017b1aee2dfcbf7dcd8ed9947558
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-19 10:30:40 -0300

    actualización movimiento

--- src/movimientoEntidades/Movimiento_Enemigo_Mejorado.java
+++ src/movimientoEntidades/Movimiento_Enemigo_Mejorado.java
@@ -1,5 +1,38 @@
 package movimientoEntidades;
 
-public class Movimiento_Enemigo_Mejorado extends Movimiento_Enemigo{
+/**
+ * Clase que modela al movimiento de un enemigo.
+ */
+public class Movimiento_Enemigo_Mejorado extends Movimiento {
+	protected final double extra = 0.5;
+	
+	/**
+	 * Constructor del movimiento de un enemigo, se inicializa con la posición inicial del mismo.
+	 * @param x Coordenada x inicial de la entidad.
+	 * @param y Coordenada y inicial de la entidad.
+	 * @param velocidad Velocidad de movimiento de la entidad.
+	 * @param alturaFrame La altura del frame del juego.
+	 */
+	public Movimiento_Enemigo_Mejorado(int x, int y,int velocidad,int alturaFrame) {
+		super(x, y, velocidad, alturaFrame);
+	}
+
+	@Override
+	public void desplazar() {
+		this.posicion.setLocation(this.posicion.x,this.posicion.y + velocidad + extra);
+		if(this.posicion.y == alturaFrame) {
+			this.posicion.setLocation(this.posicion.x,0);
+		}
+	}
+
+	@Override
+	public void moverDerecha() {
+		//No se mueve hacia la derecha.
+	}
+
+	@Override
+	public void moverIzquierda() {
+		//No se mueve hacia la izquierda.
+	}
 
 }
