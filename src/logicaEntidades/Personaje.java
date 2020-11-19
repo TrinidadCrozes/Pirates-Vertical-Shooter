472f370c2235037ba33ee07a420b977c6c9e2205
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-19 12:59:23 -0300

    Update Personaje.java

--- src/logicaEntidades/Personaje.java
+++ src/logicaEntidades/Personaje.java
@@ -1,6 +1,8 @@
 package logicaEntidades;
 
 import visitor.*;
+import movimientoEntidades.*;
+import graficaEntidades.*;
 
 /**
  * Clase que modela un personaje.
@@ -15,12 +17,12 @@ public abstract class Personaje extends Entidad {
 	 * @param movimiento
 	 * @param grafica
 	 * @param visitor
-	 * @param velocidad
 	 * @param proyectil
 	 */
-	public Personaje(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,int velocidad,Proyectil proyectil) {
+	public Personaje(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor,Proyectil proyectil) {
 		super(movimiento,grafica,visitor);
 		this.proyectil = proyectil;
+		this.vida = this.vidaMax;
 	}
 
 	public Proyectil getProyectil() {
@@ -34,8 +36,24 @@ public abstract class Personaje extends Entidad {
 	public int getVida() {
 		return vida;
 	}
-
-	public void setVida(int vida) {
-		this.vida = vida;
+	
+	public int getVidaMax() {
+		return vidaMax;
 	}
+
+	public void quitarVida(int v) {
+		this.vida = this.vida - v;
+		if(this.vida < 0) {
+			this.vida = 0;
+		}
+	}
+	
+	/**
+	 * Analiza si el personaje está vivo.
+	 * @return True si está vivo, false en caso contrario.
+	 */
+	public boolean estaVivo() {
+		return vida != 0;
+	}
+	
 }
