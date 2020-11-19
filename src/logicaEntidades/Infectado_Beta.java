6421efcd3f5ba27503711efa9c780e3ed1c45a29
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-19 12:57:11 -0300

    Update Infectado_Beta.java

--- src/logicaEntidades/Infectado_Beta.java
+++ src/logicaEntidades/Infectado_Beta.java
@@ -1,5 +1,38 @@
 package logicaEntidades;
 
-public class Infectado_Beta {
+import visitor.*;
 
+import movimientoEntidades.*;
+import graficaEntidades.*;
+/**
+ * Clase que modela un enemigo alpha.
+ */
+public class Infectado_Beta extends Infectado{
+	protected final int damageBeta = 10;
+	protected final int vidaBeta = 120;
+	
+	/**
+	 * Constructor del enemigo.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param proyectil
+	 */
+	public Infectado_Beta(Movimiento movimiento, EntidadGrafica grafica, VisitorEntidad visitor, Proyectil proyectil) {
+		super(movimiento, grafica, visitor, proyectil);
+		this.damageFisico = this.damageBeta;
+		this.vida = this.vidaBeta;
+	}
+	
+	public int getVidaMax() {
+		return vidaBeta;
+	}
+	
+	/**
+	 * Visita al enemigo alpha.
+	 * @param v Visitor.
+	 */
+	public void visitar(VisitorEntidad v) {
+		v.visit(this);
+	}
 }
