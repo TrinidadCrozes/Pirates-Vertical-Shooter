3231277fbddeced8e2b1645593f30b1b423dc5c0
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-19 12:56:52 -0300

    Update Infectado_Alpha.java

--- src/logicaEntidades/Infectado_Alpha.java
+++ src/logicaEntidades/Infectado_Alpha.java
@@ -1,19 +1,42 @@
 package logicaEntidades;
 
-import Movimiento.*;
-import visitor.VisitorEntidad;
+import visitor.*;
 
+import movimientoEntidades.*;
+import graficaEntidades.*;
+
+/**
+ * Clase que modela un enemigo alpha.
+ */
 public class Infectado_Alpha extends Infectado{
-
-	public Infectado_Alpha(Movimiento movimiento, EntidadGrafica grafica, VisitorEntidad visitor, int velocidad, Proyectil proyectil) {
-		super(movimiento, grafica, visitor, velocidad, proyectil);
+	protected final int damageAlpha = 15;
 	
+	/**
+	 * Constructor del enemigo.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param velocidad
+	 * @param proyectil
+	 */
+	public Infectado_Alpha(Movimiento movimiento, EntidadGrafica grafica, VisitorEntidad visitor, Proyectil proyectil) {
+		super(movimiento, grafica, visitor, proyectil);
+		this.damageFisico = damageAlpha;
 	}
-
-	@Override
+	
+	/**
+	 * Visita al enemigo alpha.
+	 * @param v Visitor.
+	 */
 	public void visitar(VisitorEntidad v) {
-		// TODO Auto-generated method stub
-		
+		v.visit(this);
 	}
-
+	
+	/**
+	 * Avisa si el infectado Alpha se encuentra con la vida menor o igual a 20.
+	 * @return True si la vida del infectado es menor o igual a 20, false caso contrario.
+	 */
+	public boolean estadoCritico() {
+		return (vida <= 20);
+	}
 }
