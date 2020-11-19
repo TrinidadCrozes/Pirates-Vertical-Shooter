b69774d0c6af5796885f212cad8757f2e3291b45
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-19 10:43:21 -0300

    nuevo método puedeMoverse()

--- src/movimientoEntidades/Movimiento_Proyectil_Jugador.java
+++ src/movimientoEntidades/Movimiento_Proyectil_Jugador.java
@@ -30,5 +30,10 @@ public class Movimiento_Proyectil_Jugador extends Movimiento{
 	public void moverIzquierda() {
 		//No se mueve hacia la izquierda.
 	}
+	
+	@Override
+	public boolean puedeMoverse() {
+		return !(this.posicion.y == 0);
+	}
 
 }
