227ff523bd29b18b21eeb904fd976d3d8b134830
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-18 23:44:58 -0300

    Agrego lógica del mapa

---
+++ src/logicaMapa/LogicaMapa.java
@@ -0,0 +1,31 @@
+package logicaMapa;
+
+import GUI.Juego;
+import logicaEntidades.*;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.awt.Point;
+
+/**
+ * Clase que implementa la lógica del mapa del juego.
+ */
+public class LogicaMapa extends Thread {
+	
+	protected Juego juego;
+	protected Personaje barco_pirata;
+	protected List<Infectado> barcos_infectados;
+	protected List<Premio> premios;
+	
+	
+	public LogicaMapa(Juego j) {
+		
+		this.juego = j;
+		this.barco_pirata = new Personaje();
+		this.barcos_infectados = new LinkedList<Infectado>();
+		
+	}	
+	
+	
+
+}
