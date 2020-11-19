5044506cfda21264484cd30e667a6eda285fc2ba
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-19 12:56:24 -0300

    Update Infectado.java

--- src/logicaEntidades/Infectado.java
+++ src/logicaEntidades/Infectado.java
@@ -1,16 +1,30 @@
 package logicaEntidades;
+import visitor.*;
 
-import Movimiento.*;
-import visitor.VisitorEntidad;
-
-//
-public abstract class Infectado extends Personaje {
-
-	public Infectado(Movimiento movimiento, EntidadGrafica grafica, VisitorEntidad visitor, int velocidad, Proyectil proyectil) {
-		super(movimiento, grafica, visitor, velocidad, proyectil);
-		
+import movimientoEntidades.*;
+import graficaEntidades.*;
+/**
+ * Clase que modela un enemigo.
+ */
+public abstract class Infectado extends Personaje{
+	protected int damageFisico;
+	
+	/**
+	 * Constructor del enemigo.
+	 * @param movimiento
+	 * @param grafica
+	 * @param visitor
+	 * @param proyectil
+	 */
+	public Infectado(Movimiento movimiento, EntidadGrafica grafica, VisitorEntidad visitor, Proyectil proyectil) {
+		super(movimiento, grafica, visitor, proyectil);
 	}
 	
-	
-	
+	/**
+	 * Retorna la cantidad de vida que le quita al jugador al hacer contacto.
+	 * @return cantidad de vida que quita.
+	 */
+	public int getDamageFisico() {
+		return damageFisico;
+	}
 }
