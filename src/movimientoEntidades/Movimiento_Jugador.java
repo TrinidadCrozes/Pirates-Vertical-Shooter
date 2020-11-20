aabf4ea61b1dd5a2b5c73d5389842ed129655aee
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-20 11:34:48 -0300

    Update Movimiento_Jugador.java

--- src/movimientoEntidades/Movimiento_Jugador.java
+++ src/movimientoEntidades/Movimiento_Jugador.java
@@ -1,10 +1,17 @@
 package movimientoEntidades;
 
+import logicaEntidades.Juego;
+import logicaEntidades.Proyectil;
+import logicaEntidades.Proyectil_Infectado;
+import logicaEntidades.Proyectil_Sanitario;
+import logicaEntidades.Proyectil_Sanitario_Super;
+
 /**
  * Clase que modela al movimiento del jugador.
  */
 public class Movimiento_Jugador extends Movimiento{
-
+	protected Juego juego;
+	
 	/**
 	 * Constructor del movimiento del jugador, se inicializa con la posición inicial del mismo.
 	 * @param x Coordenada x inicial de la entidad.
@@ -14,6 +21,21 @@ public class Movimiento_Jugador extends Movimiento{
 	 */
 	public Movimiento_Jugador(int x, int y, int velocidad, int alturaFrame) {
 		super(x, y, velocidad, alturaFrame);
+	}
+	
+	/**
+	 * El jugador lanza un misil.
+	 * @return Proyectil.
+	 */
+	public Proyectil atacar() {
+		Movimiento m = new Movimiento_Proyectil_Jugador(this.posicion.x,this.posicion.y,this.velocidad + 1,this.alturaFrame);
+		Proyectil_Sanitario ps;
+		if(juego.getJugador().isArmaEspecial()) {
+			ps = new Proyectil_Sanitario(m);
+		}else {
+			ps = new Proyectil_Sanitario_Super(m);
+		}
+		return ps;
 	}
 
 	@Override
@@ -30,11 +52,10 @@ public class Movimiento_Jugador extends Movimiento{
 	public void moverIzquierda() {
 		this.posicion.setLocation(this.posicion.x - velocidad,this.posicion.y);
 	}
-	
+
 	@Override
 	public boolean puedeMoverse() {
 		return true;
 	}
 
 }
-
