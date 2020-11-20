c3096c0cbbfbf228e212e075dad7fa8aacee6c64
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-20 11:27:18 -0300

    Update Entidad.java

--- src/logicaEntidades/Entidad.java
+++ src/logicaEntidades/Entidad.java
@@ -1,28 +1,19 @@
 package logicaEntidades; 
 import visitor.*;
-
 import movimientoEntidades.*;
-import graficaEntidades.*;
 
 /**
  * Clase que modela una entidad.
  */
 public abstract class Entidad {
 	protected Movimiento movimiento;
-	protected EntidadGrafica grafica;
-	protected VisitorEntidad visitor;
 	
 	/**
-	 * Constructor de la entidad.
-	 * @param movimiento
-	 * @param grafica
-	 * @param visitor
-	 * @param velocidad
+	 * Crea una entidad.
+	 * @param movimiento Movimiento del personaje.
 	 */
-	public Entidad(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor) {
+	public Entidad(Movimiento movimiento) {
 		this.movimiento = movimiento;
-		this.grafica = grafica;
-		this.visitor = visitor;
 	}
 	
 	/**
@@ -31,28 +22,20 @@ public abstract class Entidad {
 	 */
 	public abstract void visitar(VisitorEntidad v);
 
-	
+	/**
+	 * Retorna el movimiento de la entidad.
+	 * @return movimiento.
+	 */
 	public Movimiento getMovimiento() {
 		return movimiento;
 	}
-
+	
+	/**
+	 * Obtiene el movimiento de la entidad.
+	 * @param movimiento.
+	 */
 	public void setMovimiento(Movimiento movimiento) {
 		this.movimiento = movimiento;
 	}
-
-	public EntidadGrafica getGrafica() {
-		return grafica;
-	}
-
-	public void setGrafica(EntidadGrafica grafica) {
-		this.grafica = grafica;
-	}
-
-	public VisitorEntidad getVisitor() {
-		return visitor;
-	}
-
-	public void setVisitor(VisitorEntidad visitor) {
-		this.visitor = visitor;
-	}
+	
 }
