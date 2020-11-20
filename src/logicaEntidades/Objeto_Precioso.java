5cdec070075e89e470369e74a485e306e5d87a7f
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-20 11:29:09 -0300

    Update Objeto_Precioso.java

--- src/logicaEntidades/Objeto_Precioso.java
+++ src/logicaEntidades/Objeto_Precioso.java
@@ -1,9 +1,8 @@
 package logicaEntidades;
 
-import visitor.*;
+
 
 import movimientoEntidades.*;
-import graficaEntidades.*;
 
 /**
  * Clase que modela el tipo de premio objeto precioso.
@@ -11,12 +10,10 @@ import graficaEntidades.*;
 public abstract class Objeto_Precioso extends Premio{
 
 	/**
-	 * Constructor del premio.
-	 * @param movimiento
-	 * @param grafica
-	 * @param visitor
+	 * Constructor del tipo de premio objeto precioso.
+	 * @param movimiento Movimiento del premio.
 	 */
-	public Objeto_Precioso(Movimiento movimiento,EntidadGrafica grafica,VisitorEntidad visitor) {
-		super(movimiento,grafica,visitor);
+	public Objeto_Precioso(Movimiento movimiento) {
+		super(movimiento);
 	}
 }
