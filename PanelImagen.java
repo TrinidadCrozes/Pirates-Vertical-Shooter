e4637e1dd9106abcff1b65b0fdb0fefabdff2ac2
florlosta <71231076+florlosta@users.noreply.github.com>
2020-11-20 21:26:50 -0300

    jpanel con imagen (o gif) de fondo, para reutilizar

---
+++ PanelImagen.java
@@ -0,0 +1,35 @@
+package GUISudoku;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class PanelImagen extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	ImageIcon imagen;
+	String nombre;
+	
+	/**
+	 * Crea un nuevo panel con la imagen deseada de fondo. 
+	 */
+	public PanelImagen() {
+		nombre = "/ImagenesNumeros/fondoFlores.jpg";
+	}
+	
+	/**
+	 * Metodo que configura el estado interno del panel con imagen. 
+	 */
+	public void paint(Graphics g) {
+		Dimension tamaño = getSize();
+		imagen = new ImageIcon(getClass().getResource(nombre));
+		g.drawImage(imagen.getImage(), 0, 0, tamaño.width, tamaño.height, null);
+		setOpaque(false);
+		super.paint(g);
+	}
+
+}
