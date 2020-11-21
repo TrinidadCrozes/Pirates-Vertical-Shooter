173ae6344125c3692c6e3b0cecd1626d88300887
florlosta <71231076+florlosta@users.noreply.github.com>
2020-11-21 17:42:28 -0300

    Juego con fondo estatico

--- src/GUI/JFrameJuego.java
+++ src/GUI/JFrameJuego.java
@@ -2,15 +2,15 @@ package GUI;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Point;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import logicaEntidades.Entidad;
-
 public class JFrameJuego extends JFrame {
-
 	private JPanel contentPane;
 
 	/**
@@ -20,8 +20,13 @@ public class JFrameJuego extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Juego frame = new Juego();
+					//Esto lo cambie para que compile
+					JFrameJuego frame = new JFrameJuego();
+
+				//	Juego frame = new Juego();
+					
 					frame.setVisible(true);
+					frame.setResizable(false);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,14 +49,16 @@ public class JFrameJuego extends JFrame {
 	 * Inicializa el JFrame del juego.
 	 */	
 	private void initGUI() {
-  
 	  	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(450, 0, 450, 740);
-		contentPane = new JPanel();
+		//Aca en donde esta el panel con la imagen (en teoria gif) de fondo
+		contentPane = new PanelImagen();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);	
-   
+		
+		
+		  
 	}
 	
 	/**
@@ -67,5 +74,11 @@ public class JFrameJuego extends JFrame {
 	private void startGame() {
 		
 	}
+
+	public void agregarEnemigo(Point ubicacion) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
