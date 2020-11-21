beb119935b24f526613017d31f49f69d2ff2b9ff
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-21 15:15:47 -0300

    Update Nivel.java

--- src/logicaJuego/Nivel.java
+++ src/logicaJuego/Nivel.java
@@ -2,4 +2,14 @@ package logicaJuego;
 
 public abstract class Nivel {
 
+  protected int cantidadEnemigos;
+	
+	protected int getCantidadEnemigos() {
+		return cantidadEnemigos;
+	}
+
+	protected abstract int getCantidadEnemigosAlpha();
+
+	protected abstract int getCantidadEnemigosBeta();
+  
 }
