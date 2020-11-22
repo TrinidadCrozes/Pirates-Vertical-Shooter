28b0b22a82fa0b3ab577aba79cf0faf44a314372
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-22 13:18:34 -0300

    Agrego niveles(incompletos)

---
+++ src/logicaJuego/Nivel1.java
@@ -0,0 +1,23 @@
+package logicaJuego;
+
+public class Nivel1 extends Nivel {
+
+	public Nivel1() {
+		
+		super(20);
+		
+	}
+	
+	@Override
+	protected int getCantidadEnemigosAlpha() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int getCantidadEnemigosBeta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/logicaJuego/Nivel2.java
@@ -0,0 +1,24 @@
+package logicaJuego;
+
+public class Nivel2 extends Nivel {
+
+	public Nivel2() {
+		
+		super(30);
+		
+		
+	}
+	
+	@Override
+	protected int getCantidadEnemigosAlpha() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int getCantidadEnemigosBeta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
