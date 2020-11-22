38b83e62592fc4cdab229ecc7a528e4296910bfe
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-22 13:20:06 -0300

    Update Juego.java

--- src/logicaJuego/Juego.java
+++ src/logicaJuego/Juego.java
@@ -20,8 +20,8 @@ import java.awt.event.KeyEvent;
 public class Juego extends Thread {
 	
 	protected JFrameJuego gui_juego;
-	protected Jugador barco_pirata;
-	protected List<Infectado> barcos_enemigos;
+	protected Jugador jugador;
+	protected List<Infectado> enemigos;
 	protected List<Entidad> objetos_en_el_mapa;
 	protected Nivel nivel;
 	
@@ -38,41 +38,60 @@ public class Juego extends Thread {
 		
 	}
 	
-	private void inicializarJugador() {
+	/**
+	 * 
+	 */
+	public void jugar() {
 		
+		this.start();
+		
+	}
+	
+	private void inicializarJugador() {
+			
 		int mitad_JFrameJuego = (int)(this.gui_juego.getWidth()/2);
+		Point pos = new Point(mitad_JFrameJuego, 0);	
 		Movimiento_Jugador mov_jugador = new Movimiento_Jugador(mitad_JFrameJuego, 0, 4, this.gui_juego.getHeight());
-		Movimiento mov_proyectil_jugador = new Movimiento_Proyectil_Jugador( , , 10, this.gui_juego.getHeight());
+		Movimiento mov_proyectil_jugador = new Movimiento_Proyectil_Jugador( 0, 0, 10, this.gui_juego.getHeight());
 		Proyectil proyectil_jugador = new Proyectil_Sanitario(mov_proyectil_jugador);
-		this.barco_pirata = Jugador.getJugador(mov_jugador, proyectil_jugador, this);
+		this.jugador = Jugador.getJugador(mov_jugador, proyectil_jugador, this);
+		this.gui_juego.agregarJugador(this.jugador, pos);
 		
 	}
 	
 	private void inicializarEnemigos() {
 		
-		this.barcos_enemigos = new LinkedList<Infectado>();		
+		Infectado inf;
+		this.enemigos = new LinkedList<Infectado>();		
 		FabricaInfectado fabAlpha = new FabricaAlpha(this);
 		FabricaInfectado fabBeta = new FabricaBeta(this);
 		for(int i = 0; i < nivel.getCantidadEnemigosAlpha(); i++) {
-			Infectado inf = fabAlpha.crearInfectado();
-			barcos_enemigos.add(inf);
+			inf = fabAlpha.crearInfectado();
+			enemigos.add(inf);
 		}
 		for(int i = 0; i < nivel.getCantidadEnemigosBeta(); i++) {
-			Infectado inf = fabBeta.crearInfectado();
-			barcos_enemigos.add(inf);
+			inf = fabBeta.crearInfectado();
+			enemigos.add(inf);
 		}
 		
 	}
 	
 	public void run() {
 		
-		
+		boolean condicion = true;
+		try {
+			Juego.sleep(5000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		this.gui_juego.repaint();
 		
 	}
 	
 	public void keyPressed(KeyEvent arg0) {
 		
-		Movimiento mov_jugador = this.barco_pirata.getMovimiento();
+		Movimiento mov_jugador = this.jugador.getMovimiento();
 		switch(arg0.getKeyCode()) {
 			case KeyEvent.VK_LEFT: {
 				if ( mov_jugador.puedeMoverse() )
@@ -100,13 +119,12 @@ public class Juego extends Thread {
 		int pos_x;
 		int pos_lista_enemigo;
 				
-		if (this.barcos_enemigos != null) {
+		if (this.enemigos != null) {
 			pos_x = rnd.nextInt(this.gui_juego.getWidth()); 
-			pos_lista_enemigo = rnd.nextInt(this.barcos_enemigos.size()-1);
+			pos_lista_enemigo = rnd.nextInt(this.enemigos.size()-1);
 			ubicacion = new Point(pos_x, 0);
-			this.objetos_en_el_mapa.add(this.barcos_enemigos.get(pos_lista_enemigo));
-			this.barcos_enemigos.remove(pos_lista_enemigo);
-			this.gui_juego.agregarEnemigo(ubicacion);
+			this.enemigos.remove(pos_lista_enemigo);
+			this.gui_juego.agregarEnemigo(this.enemigos.get(pos_lista_enemigo), ubicacion);
 		}
 		
 	}
@@ -135,8 +153,8 @@ public class Juego extends Thread {
 	
 	public void detenerEnemigos(int duracion) {
 		
-		for(int i = 0; i < barcos_enemigos.size(); i++) {
-			Infectado e = barcos_enemigos.get(i);
+		for(int i = 0; i < enemigos.size(); i++) {
+			Infectado e = enemigos.get(i);
 			Movimiento_Enemigo mov = (Movimiento_Enemigo) e.getMovimiento();
 			mov.detener(); //No se como manejar lo de la cant de tiempo que lo detengo
 		}
@@ -145,15 +163,14 @@ public class Juego extends Thread {
 
 	public void cambiarArmaJugador(int duracion) {
 				
-		barco_pirata.activarArmaEspecial();
+		jugador.activarArmaEspecial();
 		
 	}
 
 	public Jugador getJugador() {
 		
-		return barco_pirata;
+		return jugador;
 		
 	}
 
 }
-
