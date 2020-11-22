a200254ba301dcbdd5ad1188f4b5d7b1397c50e9
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-22 13:21:07 -0300

    Update JFrameJuego.java

--- src/GUI/JFrameJuego.java
+++ src/GUI/JFrameJuego.java
@@ -4,14 +4,19 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Point;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import graficaEntidades.*;
+import logicaEntidades.*;
+import logicaJuego.Juego;
+
 public class JFrameJuego extends JFrame {
+
 	private JPanel contentPane;
+	private Juego logicaJuego;
 
 	/**
 	 * Launch the application.
@@ -20,11 +25,7 @@ public class JFrameJuego extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					//Esto lo cambie para que compile
 					JFrameJuego frame = new JFrameJuego();
-
-				//	Juego frame = new Juego();
-					
 					frame.setVisible(true);
 					frame.setResizable(false);
 				} catch (Exception e) {
@@ -49,22 +50,23 @@ public class JFrameJuego extends JFrame {
 	 * Inicializa el JFrame del juego.
 	 */	
 	private void initGUI() {
-	  	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(450, 0, 450, 740);
-		//Aca en donde esta el panel con la imagen (en teoria gif) de fondo
 		contentPane = new PanelImagen();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);	
+		setContentPane(contentPane);
 		
-		
-		  
 	}
 	
+
 	/**
 	 * Dispone cada una de las componentes en el mapa.
 	 */
 	private void initGame() {
+		
+		this.logicaJuego = new Juego(this);
 		
 	}
 	
@@ -73,12 +75,45 @@ public class JFrameJuego extends JFrame {
 	 */
 	private void startGame() {
 		
+		
+		
 	}
-
-	public void agregarEnemigo(Point ubicacion) {
-		// TODO Auto-generated method stub
+	
+	/**
+	 * Indica si la entidad pasada como parámetro está dentro de la pantalla visible o no.
+	 * @param e Entidad.
+	 * @return boolean true si está fuera de pantalla, y false en caso contrario.
+	 */
+	public boolean estaFueraDePantalla(Entidad e) {
+		
+		boolean fueraDePantalla = true;
+		return fueraDePantalla;
 		
 	}
 
+	/**
+	 * 
+	 * @param enemigo
+	 * @param ubicacion
+	 */
+	public void agregarEnemigo(Infectado enemigo, Point ubicacion) {
+		
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @param jugador
+	 * @param ubicacion
+	 */
+	public void agregarJugador(Jugador jugador, Point ubicacion) {
+		
+		EntidadGrafica graficaJugador = jugador.getEntidadGrafica();
+		JLabel labelJugador = graficaJugador.getJLabel();
+		labelJugador.setLocation(ubicacion);
+		contentPane.add(labelJugador);
+				
+	}
 
 }
