76df3be5e28a614681b7a3b0d24010c4ef8d8688
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-23 15:18:41 -0300

    Update EntidadGrafica_Enemigo_Beta.java

--- src/graficaEntidades/EntidadGrafica_Enemigo_Beta.java
+++ src/graficaEntidades/EntidadGrafica_Enemigo_Beta.java
@@ -13,10 +13,5 @@ public class EntidadGrafica_Enemigo_Beta extends EntidadGrafica{
 		redimensionar(this.etiqueta,imageIcon);
 		this.etiqueta.setIcon(imageIcon);
 	}
-
-	
-	public EntidadGrafica getEntidadGrafica() {
-		return this;
-	}
 	
 }
