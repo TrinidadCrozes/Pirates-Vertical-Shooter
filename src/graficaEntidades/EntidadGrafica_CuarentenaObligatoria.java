56c5a73e1fe9e5ecd21b2259d3b157890a7e924e
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-23 15:17:54 -0300

    Update EntidadGrafica_CuarentenaObligatoria.java

--- src/graficaEntidades/EntidadGrafica_CuarentenaObligatoria.java
+++ src/graficaEntidades/EntidadGrafica_CuarentenaObligatoria.java
@@ -13,12 +13,5 @@ public class EntidadGrafica_CuarentenaObligatoria extends EntidadGrafica{
 		redimensionar(this.etiqueta,imageIcon);
 		this.etiqueta.setIcon(imageIcon);
 	}
-
-	
-	public EntidadGrafica getEntidadGrafica() {
-		return this;
-	}
-	
-	
 	
 }
