d5e61a33ab9ed8f189bae6a2b906e66b3e968746
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-24 12:54:12 -0300

    agrego comentarios

--- src/Fabrica/FabricaBeta.java
+++ src/Fabrica/FabricaBeta.java
@@ -1,3 +1,4 @@
+
 package Fabrica;
 
 import java.awt.Point;
@@ -5,27 +6,28 @@ import java.util.Random;
 
 import logicaEntidades.Infectado;
 import logicaEntidades.Infectado_Beta;
-import logicaEntidades.Proyectil_Infectado;
 import logicaJuego.Juego;
 import movimientoEntidades.Movimiento_Enemigo;
 
+/**
+ * Clase que modela una fábrica de infectados Beta.
+ */
 public class FabricaBeta extends FabricaInfectado {
-
 	protected Movimiento_Enemigo movimiento;
-	protected Proyectil_Infectado proyectil;
-
-	public FabricaBeta(Juego juego) {
-			super(juego);
-		}
-
-	@Override
-	public Infectado crearInfectado(int anchoJFrame, int altoJFrame) { 
-		Random rnd = new Random(0);
-		int pos_x = rnd.nextInt(anchoJFrame); 
-		Point ubicacion = new Point(pos_x, 0);
-		this.movimiento = new Movimiento_Enemigo(ubicacion.x, ubicacion.y, 1, altoJFrame);
-		
-		return new Infectado_Beta(movimiento); 
+	
+	/**
+	 * Constructor de la fábrica.
+	 * @param juego
+	 */
+	public FabricaAlpha(Juego juego) {
+		super(juego);
 	}
 
+	@Override
+	public Infectado crearInfectado(int anchoJFrame, int altoJFrame) {
+		Point ubicacion = this.obtenerUbicacion(anchoJFrame);
+		this.movimiento = new Movimiento_Enemigo(ubicacion.x, ubicacion.y, velocidad, altoJFrame);
+		return new Infectado_Beta(movimiento);
+	}
+	
 }
