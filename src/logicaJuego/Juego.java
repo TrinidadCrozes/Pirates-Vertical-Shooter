485f8ee4a035ba18a611824ff03677857e94f668
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-24 00:38:19 -0300

    Update Juego.java

--- src/logicaJuego/Juego.java
+++ src/logicaJuego/Juego.java
@@ -17,7 +17,7 @@ import java.awt.event.KeyEvent;
 /**
  * Clase que implementa la lógica del juego.
  */
-public class Juego extends Thread {
+public abstract class Juego extends Thread {
 	
 	protected JFrameJuego gui_juego;
 	protected Jugador jugador;
@@ -26,151 +26,37 @@ public class Juego extends Thread {
 	protected Nivel nivel;
 	
 	/**
-	 * Inicializa la lógica del juego con los elementos que contendrá el mapa.
-	 * @param j JFrameJuego.
+	 * Inicializa el mapa del juego con las componentes necesarias.
 	 */
-	public Juego(JFrameJuego j) {
-		
-		this.gui_juego = j;
-		inicializarJugador();
-		inicializarEnemigos();		
-		this.objetos_en_el_mapa = new LinkedList<Entidad>();
-		
+	public abstract void inicializarMapa();
+	
+	/**
+	 * Comienza el juego.
+	 */
+	public abstract void jugar();
+	
+	/**
+	 * Devuelve el JFrameJuego del juego.
+	 * @return JFrameJuego.
+	 */
+	public JFrameJuego getJFrameJuego() {
+		return this.gui_juego;
 	}
 	
 	/**
-	 * 
+	 * Asigna el JFrameJuego del juego.
+	 * @param frameJuego JFrameJuego a asignar.
 	 */
-	public void jugar() {
-		
-		this.start();
-		
-	}
-	
-	private void inicializarJugador() {
-			
-		int mitad_JFrameJuego = (int)(this.gui_juego.getWidth()/2);
-		Point pos = new Point(mitad_JFrameJuego, 0);	
-		Movimiento_Jugador mov_jugador = new Movimiento_Jugador(mitad_JFrameJuego, 0, 4, this.gui_juego.getHeight());
-		Movimiento mov_proyectil_jugador = new Movimiento_Proyectil_Jugador( 0, 0, 10, this.gui_juego.getHeight());
-		Proyectil proyectil_jugador = new Proyectil_Sanitario(mov_proyectil_jugador);
-		this.jugador = Jugador.getJugador(mov_jugador, proyectil_jugador, this);
-		this.gui_juego.agregarJugador(this.jugador, pos);
-		
-	}
-	
-	private void inicializarEnemigos() {
-		
-		Infectado inf;
-		this.enemigos = new LinkedList<Infectado>();		
-		FabricaInfectado fabAlpha = new FabricaAlpha(this);
-		FabricaInfectado fabBeta = new FabricaBeta(this);
-		for(int i = 0; i < nivel.getCantidadEnemigosAlpha(); i++) {
-			inf = fabAlpha.crearInfectado();
-			enemigos.add(inf);
-		}
-		for(int i = 0; i < nivel.getCantidadEnemigosBeta(); i++) {
-			inf = fabBeta.crearInfectado();
-			enemigos.add(inf);
-		}
-		
-	}
-	
-	public void run() {
-		
-		boolean condicion = true;
-		try {
-			Juego.sleep(5000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		this.gui_juego.repaint();
-		
-	}
-	
-	public void keyPressed(KeyEvent arg0) {
-		
-		Movimiento mov_jugador = this.jugador.getMovimiento();
-		switch(arg0.getKeyCode()) {
-			case KeyEvent.VK_LEFT: {
-				if ( mov_jugador.puedeMoverse() )
-					mov_jugador.moverIzquierda();
-				break;
-			}
-			case KeyEvent.VK_RIGHT: {
-				if ( mov_jugador.puedeMoverse() )
-					mov_jugador.moverDerecha();
-				break;
-			}
-			//Falta para tirar proyectiles
-		}
-		this.gui_juego.repaint();
-		
-	}
-	
-	/**
-	 * 
-	 */
-	public void agregarEnemigo() {
-		
-		Point ubicacion;
-		Random rnd = new Random(0);
-		int pos_x;
-		int pos_lista_enemigo;
-				
-		if (this.enemigos != null) {
-			pos_x = rnd.nextInt(this.gui_juego.getWidth()); 
-			pos_lista_enemigo = rnd.nextInt(this.enemigos.size()-1);
-			ubicacion = new Point(pos_x, 0);
-			this.enemigos.remove(pos_lista_enemigo);
-			this.gui_juego.agregarEnemigo(this.enemigos.get(pos_lista_enemigo), ubicacion);
-		}
-		
-	}
-	
-	/**
-	 * 
-	 * @param e
-	 */
-	public void insertarObjeto(Entidad e) {
-		
-		
-		
-	}
-	
-	/**
-	 * 
-	 * @param e
-	 */
-	public void eliminarObjeto(Entidad e) {
-		
-		if( objetos_en_el_mapa.contains(e) ) {
-			objetos_en_el_mapa.remove(e);
-		}			
-		
-	}
-	
-	public void detenerEnemigos(int duracion) {
-		
-		for(int i = 0; i < enemigos.size(); i++) {
-			Infectado e = enemigos.get(i);
-			Movimiento_Enemigo mov = (Movimiento_Enemigo) e.getMovimiento();
-			mov.detener(); //No se como manejar lo de la cant de tiempo que lo detengo
-		}
-		
+	public void setJFrameJuego(JFrameJuego frameJuego) {
+		this.gui_juego = frameJuego;
 	}
 
-	public void cambiarArmaJugador(int duracion) {
-				
-		jugador.activarArmaEspecial();
-		
-	}
-
+	/**
+	 * Retorna el jugador del juego.
+	 * @return Jugador.
+	 */
 	public Jugador getJugador() {
-		
-		return jugador;
-		
+		return this.jugador;
 	}
 
 }
