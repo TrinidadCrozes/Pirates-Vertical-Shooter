875b672817998538ab38d5bb75365629f1bb6353
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-24 12:42:17 -0300

    Update Personaje.java

--- src/logicaEntidades/Personaje.java
+++ src/logicaEntidades/Personaje.java
@@ -7,51 +7,11 @@ import movimientoEntidades.*;
  * Clase que modela un personaje.
  */
 public abstract class Personaje extends Entidad {
-	protected int vida;
-	protected final int vidaMax = 100;
-
 	/**
 	 * Constructor del personaje.
 	 * @param movimiento Movimiento del personaje.
 	 */
 	public Personaje(Movimiento movimiento) {
 		super(movimiento);
-		this.vida = this.vidaMax;
 	}
-	
-	/**
-	 * Retorna la vida del personaje.
-	 * @return Vida.
-	 */
-	public int getVida() {
-		return vida;
-	}
-	
-	/**
-	 * Retorna la vida máxima del personaje.
-	 * @return Vida máxima.
-	 */
-	public int getVidaMax() {
-		return vidaMax;
-	}
-	
-	/**
-	 * Resta al personaje la vida indicada.
-	 * @param v Vida a restar.
-	 */
-	public void quitarVida(int v) {
-		this.vida = this.vida - v;
-		if(this.vida < 0) {
-			this.vida = 0;
-		}
-	}
-	
-	/**
-	 * Analiza si el personaje está vivo.
-	 * @return True si está vivo, false en caso contrario.
-	 */
-	public boolean estaVivo() {
-		return vida != 0;
-	}
-	
 }
