a78ae216d888837ce03cd40048d21a4a6b313ef1
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-24 12:37:13 -0300

    Update Personaje.java

--- src/logicaEntidades/Personaje.java
+++ src/logicaEntidades/Personaje.java
@@ -1,40 +1,22 @@
 package logicaEntidades;
 
+import graficaEntidades.EntidadGrafica;
 import movimientoEntidades.*;
 
 /**
  * Clase que modela un personaje.
  */
 public abstract class Personaje extends Entidad {
-	protected Proyectil proyectil;
 	protected int vida;
 	protected final int vidaMax = 100;
 
 	/**
 	 * Constructor del personaje.
 	 * @param movimiento Movimiento del personaje.
-	 * @param proyectil Proyectil del personaje.
 	 */
-	public Personaje(Movimiento movimiento,Proyectil proyectil) {
+	public Personaje(Movimiento movimiento) {
 		super(movimiento);
-		this.proyectil = proyectil;
 		this.vida = this.vidaMax;
-	}
-	
-	/**
-	 * Retorna el proyectil del personaje.
-	 * @return Proyectil del personaje.
-	 */
-	public Proyectil getProyectil() {
-		return proyectil;
-	}
-	
-	/**
-	 * Obtiene el proyectil del personaje.
-	 * @param proyectil Proyectil del personaje.
-	 */
-	public void setProyectil(Proyectil proyectil) {
-		this.proyectil = proyectil;
 	}
 	
 	/**
@@ -69,7 +51,7 @@ public abstract class Personaje extends Entidad {
 	 * @return True si está vivo, false en caso contrario.
 	 */
 	public boolean estaVivo() {
-		return vida > 0;
+		return vida != 0;
 	}
 	
 }
