f42b223a3c3bb48b2dc961ddd4ceb5c2c1b638cd
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-11-24 12:40:56 -0300

    Update FabricaInfectado.java

--- src/Fabrica/FabricaInfectado.java
+++ src/Fabrica/FabricaInfectado.java
@@ -4,16 +4,13 @@ import logicaEntidades.Infectado;
 import logicaJuego.Juego;
 
 public abstract class FabricaInfectado {
+	
 	protected Juego juego;
 	
 	public FabricaInfectado(Juego juego) {
 		this.juego = juego;
 	}
 	
-	public abstract Infectado crearInfectado();
-	
-	
-	
-	
-	
+	public abstract Infectado crearInfectado(int anchoJFrame, int altoJFrame);
+		
 }
