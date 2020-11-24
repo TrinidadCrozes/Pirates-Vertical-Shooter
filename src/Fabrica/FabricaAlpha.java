4df84f9659f1071b3e70a9b4fe814a40f62292d2
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-24 12:54:41 -0300

    agrego comentarios

--- src/Fabrica/FabricaAlpha.java
+++ src/Fabrica/FabricaAlpha.java
@@ -1,3 +1,4 @@
+
 package Fabrica;
 
 import java.awt.Point;
@@ -5,26 +6,27 @@ import java.util.Random;
 
 import logicaEntidades.Infectado;
 import logicaEntidades.Infectado_Alpha;
-import logicaEntidades.Proyectil_Infectado;
 import logicaJuego.Juego;
 import movimientoEntidades.Movimiento_Enemigo;
 
+/**
+ * Clase que modela una fábrica de infectados Alpha.
+ */
 public class FabricaAlpha extends FabricaInfectado {
-	
 	protected Movimiento_Enemigo movimiento;
-	protected Proyectil_Infectado proyectil;
 	
+	/**
+	 * Constructor de la fábrica.
+	 * @param juego
+	 */
 	public FabricaAlpha(Juego juego) {
 		super(juego);
 	}
 
 	@Override
 	public Infectado crearInfectado(int anchoJFrame, int altoJFrame) {
-		Random rnd = new Random(0);
-		int pos_x = rnd.nextInt(anchoJFrame); 
-		Point ubicacion = new Point(pos_x, 0);
-		this.movimiento = new Movimiento_Enemigo(ubicacion.x, ubicacion.y, 1, altoJFrame);
-		
+		Point ubicacion = this.obtenerUbicacion(anchoJFrame);
+		this.movimiento = new Movimiento_Enemigo(ubicacion.x, ubicacion.y, velocidad, altoJFrame);
 		return new Infectado_Alpha(movimiento);
 	}
 	
