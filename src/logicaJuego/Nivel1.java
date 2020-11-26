bea740e16a3a208407409751560c1ace109d412a
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 02:12:31 -0300

    nivel 1 nuevo

--- src/logicaJuego/Nivel1.java
+++ src/logicaJuego/Nivel1.java
@@ -1,16 +1,26 @@
+
 package logicaJuego;
 
-public class Nivel1 extends Nivel {
 
-	public Nivel1() {
-		
-		super(20);
-		this.siguienteNivel = new Nivel2();
-		
+/**
+ * Clase que modela el primer nivel del juego.
+ */
+public class Nivel1 extends Nivel {
+	protected final int cantEnemigosOleada = 10;
+	
+	/**
+	 * Constructor del nivel 1.
+	 * @param juego Juego.
+	 */
+	public Nivel1(Juego juego) {
+		super(juego);
+		this.siguienteNivel = new Nivel2(juego);
+		this.armarOleadas(cantEnemigosOleada,this.enemigos_primera_oleada);
+		this.armarOleadas(cantEnemigosOleada,this.enemigos_segunda_oleada);
 	}
 
 	@Override
-	protected Nivel getSiguienteNivel() {
+	public Nivel getSiguienteNivel() {
 		return this.siguienteNivel;
 	}
 	
