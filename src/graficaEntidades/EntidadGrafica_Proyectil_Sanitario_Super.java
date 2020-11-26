b235d108d2a2b39e2cbcdef633ca4a8e2f126742
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 10:36:54 -0300

    Update EntidadGrafica_Proyectil_Sanitario_Super.java

--- src/graficaEntidades/EntidadGrafica_Proyectil_Sanitario_Super.java
+++ src/graficaEntidades/EntidadGrafica_Proyectil_Sanitario_Super.java
@@ -9,8 +9,14 @@ public class EntidadGrafica_Proyectil_Sanitario_Super extends EntidadGrafica{
 	public EntidadGrafica_Proyectil_Sanitario_Super(int x,int y) {
 		super();
 		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/bomba_pirata_recargada.png"));
-		this.etiqueta.setBounds(x,y,30,30);
+		this.etiqueta.setBounds(x+40,y-10,20,20);
 		this.etiqueta.setIcon(imageIcon);
+		Image image = imageIcon.getImage();
+		if (image != null) {  
+			Image newimg = image.getScaledInstance(20,20,java.awt.Image.SCALE_SMOOTH);
+			imageIcon.setImage(newimg);
+			etiqueta.repaint();
+		}
 		imageIcon.setImageObserver(this.etiqueta);
 		this.etiqueta.repaint();
 	}
