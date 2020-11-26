56a957d9edd8838668964bfc69f6d299e40cc78f
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 01:59:08 -0300

    agrego coordenadas iniciales

--- src/graficaEntidades/EntidadGrafica_Jugador.java
+++ src/graficaEntidades/EntidadGrafica_Jugador.java
@@ -7,11 +7,14 @@ public class EntidadGrafica_Jugador extends EntidadGrafica{
 	/**
 	 * Constructor de la entidad gráfica del jugador.
 	 */
-	public EntidadGrafica_Jugador() {
+	public EntidadGrafica_Jugador(int x,int y) {
 		super();
-		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/jugador.gif"));
-		redimensionar(this.etiqueta,imageIcon);
+		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/barco_pirata.gif"));
+		this.etiqueta.setBounds(x,y,100,169);
 		this.etiqueta.setIcon(imageIcon);
+		imageIcon.setImageObserver(this.etiqueta);
+		this.etiqueta.repaint();
 	}
 	
 }
+
