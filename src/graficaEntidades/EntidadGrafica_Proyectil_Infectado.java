2b5a6c44386c4edad882cccf84785b5e1103252b
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 10:14:00 -0300

    para que se vea el proyectil bien

--- src/graficaEntidades/EntidadGrafica_Proyectil_Infectado.java
+++ src/graficaEntidades/EntidadGrafica_Proyectil_Infectado.java
@@ -10,8 +10,14 @@ public class EntidadGrafica_Proyectil_Infectado extends EntidadGrafica{
 	public EntidadGrafica_Proyectil_Infectado(int x,int y) {
 		super();
 		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/bomba.png"));
-		this.etiqueta.setBounds(x,y,30,30);
+		this.etiqueta.setBounds(x+31,y+140,20,20);
 		this.etiqueta.setIcon(imageIcon);
+		Image image = imageIcon.getImage();
+		if (image != null) {  
+			Image newimg = image.getScaledInstance(20,20,java.awt.Image.SCALE_SMOOTH);
+			imageIcon.setImage(newimg);
+			etiqueta.repaint();
+		}
 		imageIcon.setImageObserver(this.etiqueta);
 		this.etiqueta.repaint();
 	}
