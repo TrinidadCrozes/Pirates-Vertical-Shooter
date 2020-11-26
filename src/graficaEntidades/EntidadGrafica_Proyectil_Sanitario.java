503bec592bcb963f97c1eed92736f5a70717e5ae
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 02:00:43 -0300

    agrego coordenadas iniciales

--- src/graficaEntidades/EntidadGrafica_Proyectil_Sanitario.java
+++ src/graficaEntidades/EntidadGrafica_Proyectil_Sanitario.java
@@ -6,11 +6,12 @@ public class EntidadGrafica_Proyectil_Sanitario extends EntidadGrafica{
 	/**
 	 * Constructor de la entidad gráfica del proyectil sanitario.
 	 */
-	public EntidadGrafica_Proyectil_Sanitario() {
+	public EntidadGrafica_Proyectil_Sanitario(int x,int y) {
 		super();
 		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/bomba_pirata.png"));
-		redimensionar(this.etiqueta,imageIcon);
+		this.etiqueta.setBounds(x,y,30,30);
 		this.etiqueta.setIcon(imageIcon);
+		imageIcon.setImageObserver(this.etiqueta);
+		this.etiqueta.repaint();
 	}
-	
 }
