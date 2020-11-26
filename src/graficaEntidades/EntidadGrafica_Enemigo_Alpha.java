1643b79e3b4277fdbfd5db27614f34b73acd7acc
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 01:58:13 -0300

    agrego coordenadas iniciales

--- src/graficaEntidades/EntidadGrafica_Enemigo_Alpha.java
+++ src/graficaEntidades/EntidadGrafica_Enemigo_Alpha.java
@@ -7,11 +7,13 @@ public class EntidadGrafica_Enemigo_Alpha extends EntidadGrafica{
 	/**
 	 * Constructor de la entidad gráfica del enemigo alpha.
 	 */
-	public EntidadGrafica_Enemigo_Alpha() {
+	public EntidadGrafica_Enemigo_Alpha(int x,int y) {
 		super();
 		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/barco_infectado_rosa.png"));
-		redimensionar(this.etiqueta,imageIcon);
+		this.etiqueta.setBounds(x,y,100,169);
 		this.etiqueta.setIcon(imageIcon);
+		imageIcon.setImageObserver(this.etiqueta);
+		this.etiqueta.repaint();
 	}
 	
 }
