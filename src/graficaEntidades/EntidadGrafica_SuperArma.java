f16563dc3c409b1c748febdbdbd40c697449ef86
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 02:01:05 -0300

    agrego coordenadas iniciales

--- src/graficaEntidades/EntidadGrafica_SuperArma.java
+++ src/graficaEntidades/EntidadGrafica_SuperArma.java
@@ -6,11 +6,12 @@ public class EntidadGrafica_SuperArma extends EntidadGrafica{
 	/**
 	 * Constructor de la entidad gráfica del premio.
 	 */
-	public EntidadGrafica_SuperArma() {
+	public EntidadGrafica_SuperArma(int x,int y) {
 		super();
 		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/bandera.png"));
-		redimensionar(this.etiqueta,imageIcon);
+		this.etiqueta.setBounds(x,y,50,50);
 		this.etiqueta.setIcon(imageIcon);
+		imageIcon.setImageObserver(this.etiqueta);
+		this.etiqueta.repaint();
 	}
-	
 }
