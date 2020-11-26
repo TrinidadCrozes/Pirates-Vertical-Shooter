1a0814e8edc6e98153a8ade865c00dfc9678fdf1
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 02:15:01 -0300

    actualizo por las dudas, creo que esta igual que antes
    y si fusionamos juego con mentejuego??

--- src/logicaJuego/Juego.java
+++ src/logicaJuego/Juego.java
@@ -2,17 +2,13 @@ package logicaJuego;
 
 import logicaEntidades.*;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
 
-import Fabrica.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import GUI.JFrameJuego;
-import movimientoEntidades.*;
 
 import java.lang.Thread;
-import java.awt.Point;
-import java.awt.event.KeyEvent;
 
 /**
  * Clase que implementa la lógica del juego.
@@ -21,10 +17,17 @@ public abstract class Juego extends Thread {
 	
 	protected JFrameJuego gui_juego;
 	protected Jugador jugador;
-	protected List<Infectado> enemigos;
-	protected List<Entidad> objetos_en_el_mapa;
+	protected List<Entidad> enemigos = new ArrayList<Entidad>();;
+	protected List<Entidad> objetos_en_el_mapa = new ArrayList<Entidad>();;
 	protected Nivel nivel;
 	
+	/**
+	 * Retorna la lista de entidades en el mapa.
+	 * @return Lista de entidades.
+	 */
+	public List<Entidad> getEntidades(){
+		return objetos_en_el_mapa;
+	}
 	/**
 	 * Inicializa el mapa del juego con las componentes necesarias.
 	 */
@@ -65,4 +68,3 @@ public abstract class Juego extends Thread {
 
 
 }
-
