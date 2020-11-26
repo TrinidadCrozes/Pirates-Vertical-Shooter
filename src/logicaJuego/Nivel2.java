1ed19d0cc4d9930b5b909709557e5d5b254a3a0b
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-11-26 02:12:50 -0300

    nivel 2 nuevo

--- src/logicaJuego/Nivel2.java
+++ src/logicaJuego/Nivel2.java
@@ -1,16 +1,25 @@
+  
 package logicaJuego;
 
+/**
+ * Clase que modela el nivel 2 del juego.
+ */
 public class Nivel2 extends Nivel {
+	protected final int cantidadEnemigosOleada = 15;
 
-	public Nivel2() {
-		
-		super(30);
+	/**
+	 * Constructor del nivel.
+	 * @param juego Juego.
+	 */
+	public Nivel2(Juego juego) {
+		super(juego);
 		this.siguienteNivel = null;
-		
+		this.armarOleadas(cantidadEnemigosOleada,this.enemigos_primera_oleada);
+		this.armarOleadas(cantidadEnemigosOleada,this.enemigos_segunda_oleada);
 	}
 
 	@Override
-	protected Nivel getSiguienteNivel() {
+	public Nivel getSiguienteNivel() {
 		return this.siguienteNivel;
 	}
 
