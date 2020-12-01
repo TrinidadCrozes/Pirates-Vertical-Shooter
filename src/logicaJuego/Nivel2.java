f4167a348c0a9eb775f0c921eecb36bc0febc846
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-12-01 11:28:21 -0300

    mejoras en la lógica

--- src/logicaJuego/Nivel2.java
+++ src/logicaJuego/Nivel2.java
@@ -1,26 +1,54 @@
-  
+    
 package logicaJuego;
+
+import logicaEntidades.Entidad;
 
 /**
  * Clase que modela el nivel 2 del juego.
  */
 public class Nivel2 extends Nivel {
-	protected final int cantidadEnemigosOleada = 15;
-
+	protected final int cantEnemigosOleada = 15;
+	protected int cantEnemigosMapa = 0;
+	
 	/**
-	 * Constructor del nivel.
+	 * Constructor del nivel 2.
 	 * @param juego Juego.
 	 */
 	public Nivel2(Juego juego) {
 		super(juego);
 		this.siguienteNivel = null;
-		this.armarOleadas(cantidadEnemigosOleada,this.enemigos_primera_oleada);
-		this.armarOleadas(cantidadEnemigosOleada,this.enemigos_segunda_oleada);
+		this.armarOleadas(cantEnemigosOleada,this.enemigos_primera_oleada);
+		this.armarOleadas(cantEnemigosOleada,this.enemigos_segunda_oleada);
 	}
 
 	@Override
 	public Nivel getSiguienteNivel() {
 		return this.siguienteNivel;
 	}
-
+	
+	@Override
+	public Entidad getEnemigo(int oleada) {
+		Entidad enemigo = null;
+		if(oleada == 1 && !enemigos_primera_oleada.isEmpty()) {
+			enemigo = this.enemigos_primera_oleada.pop();
+			cantEnemigosMapa++;
+		}else {
+			if(oleada == 2 && !enemigos_segunda_oleada.isEmpty()) {
+				enemigo = this.enemigos_segunda_oleada.pop();
+				cantEnemigosMapa++;
+			}
+		}
+		return enemigo;
+	}
+	
+	@Override
+	public boolean todosEnJuego() {
+		return this.cantEnemigosMapa == this.cantEnemigosOleada;
+	}
+	
+	@Override
+	public void nadieEnJuego() {
+		this.cantEnemigosMapa = 0;
+	}
+	
 }
