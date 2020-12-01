0545a5f2b1e8429d0faaf2f0f400a4abe59e7396
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-12-01 15:05:44 -0300

    Update EntidadGrafica_CuarentenaObligatoria.java

--- src/graficaEntidades/EntidadGrafica_CuarentenaObligatoria.java
+++ src/graficaEntidades/EntidadGrafica_CuarentenaObligatoria.java
@@ -1,19 +1,39 @@
 package graficaEntidades;
 
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
 
 public class EntidadGrafica_CuarentenaObligatoria extends EntidadGrafica{
-	
+	protected final int desplazamientoX = 31;
+	protected final int desplazamientoY = 140;
 	/**
 	 * Constructor de la entidad gráfica del premio.
 	 */
 	public EntidadGrafica_CuarentenaObligatoria(int x,int y) {
 		super();
 		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/brujula.png"));
-		this.etiqueta.setBounds(x,y,50,50);
+		acomodarEtiqueta(x,y);
 		this.etiqueta.setIcon(imageIcon);
+		Image image = imageIcon.getImage();
+		if (image != null) {  
+			Image newimg = image.getScaledInstance(20,20,java.awt.Image.SCALE_SMOOTH);
+			imageIcon.setImage(newimg);
+			etiqueta.repaint();
+		}
 		imageIcon.setImageObserver(this.etiqueta);
 		this.etiqueta.repaint();
+	}
+	
+	@Override
+	public void acomodarEtiqueta(int x, int y) {
+		this.etiqueta.setBounds(x+desplazamientoX,y+desplazamientoY,50,50);
+	}
+
+	@Override
+	public void modificarEtiqueta() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
