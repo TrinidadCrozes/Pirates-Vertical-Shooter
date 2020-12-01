5081c1915d794f9466b14a2ac28d52a5efbc55be
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-12-01 15:06:51 -0300

    Update EntidadGrafica_Pocion.java

--- src/graficaEntidades/EntidadGrafica_Pocion.java
+++ src/graficaEntidades/EntidadGrafica_Pocion.java
@@ -1,18 +1,39 @@
 package graficaEntidades;
 
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
 
 public class EntidadGrafica_Pocion extends EntidadGrafica {
-	
+	protected final int desplazamientoX = 31;
+	protected final int desplazamientoY = 140;
 	/**
 	 * Constructor de la entidad gráfica del premio.
 	 */
 	public EntidadGrafica_Pocion(int x,int y) {
+		super();
 		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/pocion.png"));
-		this.etiqueta.setBounds(x,y,50,50);
+		acomodarEtiqueta(x,y);
 		this.etiqueta.setIcon(imageIcon);
+		Image image = imageIcon.getImage();
+		if (image != null) {  
+			Image newimg = image.getScaledInstance(20,30,java.awt.Image.SCALE_SMOOTH);
+			imageIcon.setImage(newimg);
+			etiqueta.repaint();
+		}
 		imageIcon.setImageObserver(this.etiqueta);
 		this.etiqueta.repaint();
+	}
+	
+	@Override
+	public void acomodarEtiqueta(int x, int y) {
+		this.etiqueta.setBounds(x+desplazamientoX,y+desplazamientoY,50,50);
+	}
+
+	@Override
+	public void modificarEtiqueta() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
