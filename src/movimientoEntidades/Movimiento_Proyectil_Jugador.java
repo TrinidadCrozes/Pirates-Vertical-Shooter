ee8a934fac7f01723c4f0f8fa3f462f59561b45f
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-12-10 18:41:01 -0300

    velocidad responsabilidad del movimiento

--- src/movimientoEntidades/Movimiento_Proyectil_Jugador.java
+++ src/movimientoEntidades/Movimiento_Proyectil_Jugador.java
@@ -7,21 +7,22 @@ import logicaEntidades.Proyectil;
  * Clase que modela el movimiento de los proyectiles del jugador.
  */
 public class Movimiento_Proyectil_Jugador extends Movimiento{
-
+	protected final int velocidadProyectilJugador = 4;
+	
 	/**
 	 * Constructor del movimiento de un proyectil, se inicializa con la posición inicial del mismo.
 	 * @param x Coordenada x inicial de la entidad.
 	 * @param y Coordenada y inicial de la entidad.
-	 * @param velocidad Velocidad de movimiento de la entidad.
 	 * @param alturaFrame La altura del frame del juego.
 	 */
-	public Movimiento_Proyectil_Jugador(int x, int y, int velocidad, int alturaFrame) {
-		super(x, y, velocidad, alturaFrame);
+	public Movimiento_Proyectil_Jugador(int x, int y, int alturaFrame) {
+		super(x, y, alturaFrame);
+		this.setVelocidad(velocidadProyectilJugador);
 	}
 
 	@Override
 	public void desplazar() {
-		this.posicion.setLocation(this.posicion.x,this.posicion.y - velocidad);
+		this.posicion.setLocation((int)this.posicion.getX(),(int)this.posicion.getY() - velocidad);
 	}
 
 	@Override
@@ -36,18 +37,16 @@ public class Movimiento_Proyectil_Jugador extends Movimiento{
 
 	@Override
 	public boolean puedeMoverse() {
-		return !(this.posicion.y == 0);
+		return !(this.posicion.y <= 0);
 	}
 
 	@Override
 	public Proyectil atacar() {
-		// no hacer nada
 		return null;
 	}
 
 	@Override
 	public Premio lanzarPremio() {
-		//Los proyectiles no lanzan premios
 		return null;
 	}
 
