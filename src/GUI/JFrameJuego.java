6288247ed30cb5065fc6698092abc4c9dbd9f523
TrinidadCrozes <71290035+TrinidadCrozes@users.noreply.github.com>
2020-12-10 20:22:39 -0300

    Actualizo JFrameJuego

--- src/GUI/JFrameJuego.java
+++ src/GUI/JFrameJuego.java
@@ -1,9 +1,11 @@
 package GUI;
 
 import java.awt.EventQueue;
+
 import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.FileNotFoundException;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -12,22 +14,34 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import audio.Musica;
 import graficaEntidades.*;
+import javazoom.jl.decoder.JavaLayerException;
 import logicaEntidades.*;
 import logicaJuego.*;
 
-
+/**
+ * Clase que modela la gráfica del juego.
+ */
 @SuppressWarnings("serial")
 public class JFrameJuego extends JFrame {
-	private final int ancho = 330;
+	private final int ancho = 340;
 	private final int altura = 530;
+	
+	private Musica musica;
 	
 	private JPanel contentPane;
 	private JLabel labelFondo;
+	private JPanel panelNivelOleada;
+	
 	private Juego logicaJuego;
+	
+	private Grafica_VidaJugador[] labelsVida;
+	private Grafica_NivelOleada[] labelsNivelOleada;
+	private String[] imagenes = {"/IMG/nivel.png","/IMG/1.png","/IMG/oleada.png","/IMG/2.png"};
 
 	/**
-	 * Launch the application.
+	 * Ejecuta la aplicación.
 	 */
 	public static void main(String[] args) {
 		SplashScreen splash = new SplashScreen(3000);
@@ -46,7 +60,7 @@ public class JFrameJuego extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Crea el frame.
 	 */
 	public JFrameJuego() {
 		initGUI();
@@ -68,16 +82,14 @@ public class JFrameJuego extends JFrame {
 		contentPane.setLayout(null);
 	
 		labelFondo = new JLabel();
-		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/fondito.gif"));
+		ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/IMG/background.gif"));
 		labelFondo.setBounds(0, 0, 450, 740);
 		labelFondo.setIcon(imageIcon);
 		imageIcon.setImageObserver(labelFondo);
 		labelFondo.repaint();
 	
 		contentPane.add(labelFondo);
-	
 	}
-	
 
 	/**
 	 * Dispone cada una de las componentes en el mapa.
@@ -87,11 +99,24 @@ public class JFrameJuego extends JFrame {
 		this.logicaJuego = new Juego();
 		this.logicaJuego.setJFrameJuego(this);
 		this.logicaJuego.inicializarMapa();
-		
+
 		addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent arg0) {
-				mover(arg0);
+				if (arg0.getKeyCode() == KeyEvent.VK_R) {
+					try {
+						musica = new Musica();
+					} catch (JavaLayerException | FileNotFoundException e) {
+						e.printStackTrace();
+					}
+					musica.audioOn();
+				} else {
+					if (arg0.getKeyCode() == KeyEvent.VK_F) {
+						musica.audioOff();
+					} else
+						mover(arg0);
+				}
+				
 			}
 		});
 	}
@@ -153,23 +178,7 @@ public class JFrameJuego extends JFrame {
 	public int getHeight() {
 		return this.altura;
 	}
-	
-	/**
-	 * Indica si la entidad pasada como parámetro está dentro de la pantalla visible o no.
-	 * @param e Entidad.
-	 * @return boolean true si está fuera de pantalla, y false en caso contrario.
-	 */
-	public boolean estaFueraDePantalla(Entidad e) {
-		
-		boolean fueraDePantalla = false;
-		int pos_x = e.getEntidadGrafica().getJLabel().getLocation().x;
-		int pos_y = e.getEntidadGrafica().getJLabel().getLocation().y;
-		if( pos_x > this.getWidth() || pos_x < 0 || pos_y < 0 || pos_y > 740 )
-			fueraDePantalla = true;
-		return fueraDePantalla;
-		
-	}
-	
+
 	/**
 	 * Agrega gráficamente una entidad al mapa.
 	 * @param entidad Entidad a agregar.
@@ -191,4 +200,109 @@ public class JFrameJuego extends JFrame {
 		labelFondo.remove(label);
 		labelFondo.repaint();
 	}
+
+	/**
+	 * Agrega gráficamente la vida del jugador.
+	 * @param j Jugador.
+	 */
+	public void agregarVidaJugador(Jugador j) {
+		labelsVida = new Grafica_VidaJugador[5];
+	
+		int ubicacion = this.ancho - 40;
+		
+		for (int i = 0; i < labelsVida.length; i++) {
+			labelsVida[i] = new Grafica_VidaJugador(ubicacion, 10);
+			labelFondo.add(labelsVida[i].getJLabel());
+			labelFondo.repaint();
+			ubicacion+=25;
+		}
+		
+		actualizarVidaJugador(j);
+	}
+	
+	/**
+	 * Actualiza gráficamente la vida del jugador.
+	 * @param j Jugador.
+	 */
+	public void actualizarVidaJugador(Jugador j) {
+		int vida = j.getVida();
+		
+		labelsVida[0].agregarVida();
+		labelsVida[1].agregarVida();
+		labelsVida[2].agregarVida();
+		labelsVida[3].agregarVida();
+		labelsVida[4].agregarVida();
+		
+		if (vida <= 0) {
+			labelsVida[0].sacarVida();
+			labelsVida[1].sacarVida();
+			labelsVida[2].sacarVida();
+			labelsVida[3].sacarVida();
+			labelsVida[4].sacarVida();
+		}else {
+			if (vida > 0 && vida <= 20) {
+				labelsVida[1].sacarVida();
+				labelsVida[2].sacarVida();
+				labelsVida[3].sacarVida();
+				labelsVida[4].sacarVida();
+			}else {
+				if (vida > 20 && vida <= 40) {
+					labelsVida[2].sacarVida();
+					labelsVida[3].sacarVida();
+					labelsVida[4].sacarVida();
+				}else {
+					if(vida > 40 && vida <= 60) {
+						labelsVida[3].sacarVida();
+						labelsVida[4].sacarVida();
+					}else {
+						if(vida > 60 && vida <= 80) {
+							labelsVida[4].sacarVida();
+						}
+					}
+				}
+			}
+		}
+		
+		labelFondo.repaint();
+	}
+	
+	/**
+	 * Muestra gráficamente en que nivel y oleada se encuentra el juego.
+	 * @param nivel int que indica el nivel.
+	 * @param oleada int que indica la oleada.
+	 */
+	public void mostrarNivelOleada(int nivel, int oleada) {
+		panelNivelOleada = new JPanel();
+		panelNivelOleada.setOpaque(false);
+		panelNivelOleada.setBounds(10, 7, 250, 50);
+		labelsNivelOleada = new Grafica_NivelOleada[4];
+		for(int i = 0; i < labelsNivelOleada.length; i++) {
+			labelsNivelOleada[i] = new Grafica_NivelOleada(imagenes[i]);
+			panelNivelOleada.add(labelsNivelOleada[i].getJLabel());
+		}
+		labelFondo.add(panelNivelOleada);
+		actualizarNivelOleada(nivel,oleada);
+	}
+	
+	/**
+	 * Actualiza el nivel y oleada en que se encuentra.
+	 * @param nivel int nuevo nivel.
+	 * @param oleada int nueva oleada.
+	 */
+	public void actualizarNivelOleada(int nivel,int oleada) {
+		switch(nivel) {
+		case 1: labelsNivelOleada[1].modificarEtiqueta(imagenes[1]);
+				break;
+		default : labelsNivelOleada[1].modificarEtiqueta(imagenes[3]);
+				break;
+		}
+		
+		switch(oleada) {
+			case 1: labelsNivelOleada[3].modificarEtiqueta(imagenes[1]); 
+					break;
+			default : labelsNivelOleada[3].modificarEtiqueta(imagenes[3]);
+					break;
+		}
+	}
+
 }
