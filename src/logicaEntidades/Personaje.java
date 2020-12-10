f0fecdc3ccaf6813161f673b6a326d06c78c16ef
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2020-12-10 18:52:41 -0300

    ahora conoce al juego

--- src/logicaEntidades/Personaje.java
+++ src/logicaEntidades/Personaje.java
@@ -1,21 +1,22 @@
 package logicaEntidades;
 
+import logicaJuego.Juego;
 import movimientoEntidades.*;
-
-
-
 
 /**
  * Clase que modela un personaje.
  */
 public abstract class Personaje extends Entidad {
-
+	protected Juego juego;
+	
 	/**
 	 * Constructor del personaje.
 	 * @param movimiento Movimiento del personaje.
+	 * @param juego Juego.
 	 */
-	public Personaje(Movimiento movimiento) {
+	public Personaje(Movimiento movimiento,Juego juego) {
 		super(movimiento);
+		this.juego = juego;
 	}
 	
 }
